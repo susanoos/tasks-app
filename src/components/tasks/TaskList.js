856f5439457d5f69e5373797cf27d6f740970175
David <escxbar@gmail.com>
2023-02-23 17:33:38 -0800

    added ability to add tasks to firebase

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -2,6 +2,25 @@ import React, { Fragment } from "react";
 import classes from "../../styles/TaskList.module.css";
 
 const TaskList = ({ tasks }) => {
+  const deleteTask = async (id) => {
+    const response = await fetch(
+      `https://task-app-32c81-default-rtdb.firebaseio.com/tasks.json/${id}`,
+      {
+        method: "DELETE",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+  };
+  const deleteHandler = (e) => {
+    e.preventDefault();
+    const id = e.target.id;
+    deleteTask(id);
+    console.log(id);
+  };
   const tasksList = tasks
     .map((task) => {
       return (
@@ -11,7 +30,13 @@ const TaskList = ({ tasks }) => {
               {task.task}
               <div className={classes.btn__container}>
                 <button className={classes.btn__tasks}>Edit</button>
-                <button className={classes.btn__tasks}>Delete</button>
+                <button
+                  className={classes.btn__tasks}
+                  onClick={deleteHandler}
+                  id={task.id}
+                >
+                  Delete
+                </button>
               </div>
             </li>
           </div>
@@ -20,7 +45,13 @@ const TaskList = ({ tasks }) => {
     })
     .reverse();
 
-  return <ul className={classes.form__container}>{tasksList}</ul>;
+  const showTaskList =
+    tasks.length !== 0 ? (
+      tasksList
+    ) : (
+      <p className={classes.add__tasks__text}>Add some tasks</p>
+    );
+  return <ul className={classes.form__container}>{showTaskList}</ul>;
 };
 
 export default TaskList;
